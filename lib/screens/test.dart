415eba65a57c7bf9676065d7c170534614dcb1c1
fadhilahmargi <fadhilahmargi@gmail.com>
2024-04-27 13:19:14 +0700

    nambah halaman exercise (test.dart)

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -1,17 +1,207 @@
 import 'package:flutter/material.dart';
 
-class TestPage extends StatefulWidget {
+class TestPage extends StatelessWidget {
   const TestPage({super.key});
 
   @override
-  State<TestPage> createState() => _TestPageState();
-}
+  Widget build(BuildContext context) {
+    double containerWidth = MediaQuery.of(context).size.width - 30;
 
-class _TestPageState extends State<TestPage> {
-  @override
-   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(child: Text("Test Page"),),
+      backgroundColor: const Color(0xFF0D0443),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.arrow_back_ios_new,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                  ),
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8),
+                      child: Text(
+                        "EXERCISE",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              height: MediaQuery.of(context).size.height / 1,
+              width: double.infinity,
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 15,
+                right: 15,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.9),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const SizedBox(height: 10),
+                  Container(
+                    width: containerWidth,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF0D0443),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.9),
+                          spreadRadius: 2,
+                          blurRadius: 10,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(15),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                          child: Text(
+                            'Vocabulary is the cornerstone of\ncommunication, shaping our\nthoughts into articulate\nexpressions.',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Image.asset(
+                          'assets/images/writing.png',
+                          width: 120,
+                          height: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 1),
+                    child: Column(
+                      children: [
+                        _buildMenuItem(
+                          context,
+                          "Listening",
+                          'assets/images/listening-logo.png',
+                        ),
+                        const SizedBox(height: 10),
+                        _buildMenuItem(
+                          context,
+                          "Speaking",
+                          'assets/images/speaking-logo.png',
+                        ),
+                        const SizedBox(height: 10),
+                        _buildMenuItem(
+                          context,
+                          "Reading",
+                          'assets/images/reading-logo.png',
+                        ),
+                        const SizedBox(height: 10),
+                        _buildMenuItem(
+                          context,
+                          "Writing",
+                          'assets/images/writing-logo.png',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(
+    BuildContext context,
+    String title,
+    String imagePath,
+  ) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 10,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Image.asset(
+            imagePath,
+            width: 50, // Adjust width as needed
+            height: 50, // Adjust height as needed
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 2),
+                Text(
+                  "5 Level 3 Done",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey[600],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Icon(Icons.arrow_drop_down, color: Colors.black),
+        ],
+      ),
     );
   }
 }
