db45201b3fbcfa5928423ca02289715278cee22a
unknown <fauziyahdewi21@gmail.com>
2024-04-29 14:20:48 +0700

    exercise semangat

---
+++ lib/pages/exercise/exercise_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExercisePage extends StatefulWidget {
+  const ExercisePage({super.key});
+
+  @override
+  State<ExercisePage> createState() => _ExercisePageState();
+}
+
+class _ExercisePageState extends State<ExercisePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('exercise page'),
+    );
+  }
+}
