2dae0df5dadf2ad84ee10844ff9471169c64c83f
unknown <fauziyahdewi21@gmail.com>
2024-04-29 12:30:50 +0700

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:learn_toefl/pages/home/home_page.dart';
-import 'package:learn_toefl/pages/exercise.dart';
-import 'package:learn_toefl/pages/profile.dart';
 import 'package:learn_toefl/widget/bottom_navigation.dart';
 
 void main() {
