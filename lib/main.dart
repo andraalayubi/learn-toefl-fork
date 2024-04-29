7c2d5e5597b7ac15b0b7869f7da295761723d096
fadhilahmargi <fadhilahmargi@gmail.com>
2024-04-29 12:34:05 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:learn_toefl/widget/bottom_navigation.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:learn_toefl/screens/homepage.dart';
+import 'package:learn_toefl/screens/exercise.dart';
+import 'package:learn_toefl/screens/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,8 +26,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    Container(color: Colors.white, child: Center(child: Text('Page 11'))),
+    Container(color: Colors.white, child: Center(child: Text('Page 2'))),
+    Container(color: Colors.white, child: Center(child: Text('Page 3'))),
+  ];
+
   @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text('Curved Navigation Bar Example'),
+  //     ),
+  //     body: _pages[_currentIndex],
+  //     bottomNavigationBar: CurvedNavigationBar(
+  //       height: 75,
+  //       color: Color(0xFF0D0443),
+  //       backgroundColor: Color.fromARGB(255, 255, 255, 255),
+  //       items: <Widget>[
+  //         Icon(
+  //           Icons.home,
+  //           size: 40,
+  //           color: Colors.white,
+  //         ),
+  //         Icon(
+  //           Icons.task_alt_sharp,
+  //           size: 40,
+  //           color: Colors.white,
+  //         ),
+  //         Icon(
+  //           Icons.person,
+  //           size: 40,
+  //           color: Colors.white,
+  //         ),
+  //       ],
+  //       onTap: (index) {
+  //         setState(() {
+  //           _currentIndex = index;
+  //         });
+  //       },
+  //     ),
+  //   );
+  // }
   Widget build(BuildContext context) {
-    return const BottomNavigation();
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Curved Navigation Bar Example'),
+      // ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          HomePage(),
+          Exercise(),
+          ProfilPage(),
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 75,
+        color: Color(0xFF0D0443),
+        backgroundColor: Colors.transparent,
+        items: <Widget>[
+          Icon(
+            Icons.home,
+            size: 40,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.task_alt_sharp,
+            size: 40,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.person,
+            size: 40,
+            color: Colors.white,
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
