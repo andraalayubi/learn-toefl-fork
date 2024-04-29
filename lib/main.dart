e80c5aa8fe613590d10d2495d4bda1ccc3e306d9
unknown <fauziyahdewi21@gmail.com>
2024-04-29 12:34:58 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
+    return const MaterialApp(
+      home: BottomNavigation(),
       debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const BottomNavigation();
   }
 }
