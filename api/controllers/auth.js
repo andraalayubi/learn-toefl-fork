8d6467e65f2f757f143288bb780e245fc9ad7472
andraalayubi <59492315+andraalayubi@users.noreply.github.com>
2024-05-21 15:03:12 +0700

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -21,6 +21,8 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
+    console.log(email);
+    console.log(password);
     try {
         const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
         if (result.rows.length === 0) {
