12da42a34cf71628f4bc59b67504876b7f8b15b2
fadhilahmargi <fadhilahmargi@gmail.com>
2024-05-26 04:39:32 +0700

    update subexercise

--- lib/pages/admin/exercise/subexercise_page.dart
+++ lib/pages/admin/exercise/subexercise_page.dart
@@ -8,21 +8,31 @@ class SubExercisePage extends StatefulWidget {
 }
 
 class _SubExercisePageState extends State<SubExercisePage> {
+  String? _selectedLevel;
+  int _correctOptionIndex = 0;
+  TextEditingController _questionController = TextEditingController();
+  List<TextEditingController> _optionControllers = [
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+    TextEditingController(),
+  ];
+  bool _fieldsFilled = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(200.0), // Set the height of the AppBar
+        preferredSize: const Size.fromHeight(200.0),
         child: Container(
           height: 200.0,
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                  'assets/images/appbar-exercise.png'), // Path gambar yang diperbaiki
+            image: const DecorationImage(
+              image: AssetImage('assets/images/appbar-exercise.png'),
               fit: BoxFit.cover,
             ),
-            boxShadow: [
-              BoxShadow(blurRadius: 50.0),
+            boxShadow: const [
+              BoxShadow(blurRadius: 30.0),
             ],
             borderRadius: BorderRadius.vertical(
               bottom: Radius.elliptical(
@@ -36,8 +46,7 @@ class _SubExercisePageState extends State<SubExercisePage> {
             elevation: 0,
             title: const Text(
               'WRITING',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
             leading: Padding(
               padding: const EdgeInsets.only(left: 22.0),
@@ -51,188 +60,213 @@ class _SubExercisePageState extends State<SubExercisePage> {
           ),
         ),
       ),
-      body: _buildExerciseMenu(context),
-    );
-  }
-
-  Widget _buildExerciseMenu(BuildContext context) {
-    return Column(
-      children: [
-        _buildMenuItem(
-          "Listening",
-          'assets/images/iconVideo_1.png',
-          [
-            "Level 1\n10 Questions",
-            "Level 2\n10 Questions",
-            "Level 3\n10 Questions",
-            "Level 4\n10 Questions",
-            "Level 5\n10 Questions",
-          ],
-          5,
-          10,
-          context,
-        ),
-        const SizedBox(height: 10),
-        _buildMenuItem(
-          "Speaking",
-          'assets/images/iconVideo_2.png',
-          [
-            "Level 1\n10 Questions",
-            "Level 2\n10 Questions",
-            "Level 3\n10 Questions",
-            "Level 4\n10 Questions",
-            "Level 5\n10 Questions",
-          ],
-          3,
-          10,
-          context,
-        ),
-        const SizedBox(height: 10),
-        _buildMenuItem(
-          "Reading",
-          'assets/images/iconVideo_3.png',
-          [
-            "Level 1\n10 Questions",
-            "Level 2\n10 Questions",
-            "Level 3\n10 Questions",
-            "Level 4\n10 Questions",
-            "Level 5\n10 Questions",
-          ],
-          8,
-          10,
-          context,
-        ),
-        const SizedBox(height: 10),
-        _buildMenuItem(
-          "Writing",
-          'assets/images/iconVideo_4.png',
-          [
-            "Level 1\n10 Questions",
-            "Level 2\n10 Questions",
-            "Level 3\n10 Questions",
-            "Level 4\n10 Questions",
-            "Level 5\n10 Questions",
-          ],
-          4,
-          10,
-          context,
-        ),
-      ],
-    );
-  }
-
-  Widget _buildMenuItem(
-    String title,
-    String imagePath,
-    List<String> levels,
-    int levelDone,
-    int totalLevels,
-    BuildContext context,
-  ) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5.0),
-      child: Card(
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(width: 1.0, color: Colors.black),
-              borderRadius: BorderRadius.circular(12)),
-          child: ExpansionTile(
-            shape: const Border(),
-            title: Row(
-              children: [
-                Image.asset(
-                  imagePath,
-                  width: 50,
-                  height: 50,
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                  border: Border.all(color: Colors.black),
                 ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    const Row(
+                      children: [
+                        Text(
+                          "Level",
+                          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
                         ),
+                      ],
+                    ),
+                    const SizedBox(height: 2.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      const SizedBox(height: 2),
-                      RichText(
-                        text: TextSpan(
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Color.fromARGB(255, 0, 0, 0),
-                          ),
-                          children: [
-                            TextSpan(
-                                text: '$levelDone Level $totalLevels Done'),
-                            const TextSpan(
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          menuMaxHeight: 200,
+                          underline: null,
+                          isExpanded: true,
+                          value: _selectedLevel,
+                          hint: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              "Choose level",
                               style: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic,
                               ),
                             ),
-                          ],
+                          ),
+                          items: <String>['Level 1', 'Level 2', 'Level 3']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(value),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _selectedLevel = newValue;
+                            });
+                          },
+                          icon: const Padding(
+                            padding: EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.keyboard_arrow_down),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            children: levels.map((level) {
-              final parts = level.split('\n');
-              return ListTile(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      parts[0], // "Level 1"
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      parts[1], // "10 Questions"
-                      style: const TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey,
                       ),
                     ),
                   ],
                 ),
-                trailing: Container(
-                  width: 58,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 24, 11, 70),
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  child: const Text(
-                    '0%', // Skor contoh
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 9,
-                      color: Colors.white,
+              ),
+              const SizedBox(height: 20.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: const Offset(0, 2),
                     ),
-                  ),
+                  ],
+                  border: Border.all(color: Colors.black),
                 ),
-                // onTap: () {
-                //   Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //       builder: (context) => const ExerciseQuestionAnswer(),
-                //     ),
-                //   );
-                // },
-              );
-            }).toList(),
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Question 1",
+                      style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _questionController,
+                            decoration: InputDecoration(
+                              hintText: 'Type your question here',
+                              hintStyle: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                color: Colors.grey,
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 10.0,
+                                vertical: 10.0,
+                              ),
+                            ),
+                          ),
+                          for (int i = 0; i < 4; i++)
+                            _buildOptionInput('Option ${i + 1}', _optionControllers[i]),
+                          Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Correct Answer :',
+                                  style: TextStyle(fontSize: 16.0),
+                                ),
+                                const SizedBox(width: 10),
+                                DropdownButton<int>(
+                                  value: _correctOptionIndex,
+                                  onChanged: (int? newValue) {
+                                    setState(() {
+                                      _correctOptionIndex = newValue!;
+                                    });
+                                  },
+                                  items: List.generate(4, (index) => index)
+                                      .map<DropdownMenuItem<int>>(
+                                        (int value) => DropdownMenuItem<int>(
+                                          value: value,
+                                          child: Text('Option ${value + 1}'),
+                                        ),
+                                      )
+                                      .toList(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: ElevatedButton(
+          onPressed: () {
+            // Check if all required fields are filled
+            if (_selectedLevel != null &&
+                _questionController.text.isNotEmpty &&
+                _optionControllers.every((controller) => controller.text.isNotEmpty)) {
+              // If all fields are filled, show the notification
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('The new question is saved successfully'),
+                  duration: Duration(seconds: 2),
+                ),
+              );
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFF0D0443),
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+          ),
+          child: Text(
+            'Save',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionInput(String label, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: label,
+          hintStyle: TextStyle(
+            fontStyle: FontStyle.italic,
+            color: Colors.grey,
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+            vertical: 10.0,
+          ),
+          border: OutlineInputBorder(),
         ),
       ),
     );
