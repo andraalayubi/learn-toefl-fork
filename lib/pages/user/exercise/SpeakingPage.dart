50511d36d1d5d7788d6d605f3bae84c5c1c28bf7
andraalayubi <andraalayubi@gmail.com>
2024-05-27 00:26:40 +0700

    speaking fetch

--- lib/pages/user/exercise/SpeakingPage.dart
+++ lib/pages/user/exercise/SpeakingPage.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:learn_toefl/models/question.dart';
 import 'package:learn_toefl/utilities.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-import 'package:speech_to_text/speech_to_text_provider.dart';
 
 class SpeakingTest extends StatefulWidget {
   final int questionGroupId;
@@ -14,8 +13,8 @@ class SpeakingTest extends StatefulWidget {
 }
 
 class _SpeakingTestState extends State<SpeakingTest> {
-  String providedText =
-      'The flower petals are very large in size and release odors such as the smell of carrion Flower buds that have medicinal properties resulting in the passage of this flower are often looted This plant is native habitat on the island of Sumatra especially in Bengkulu Jambi and South Sumatra';
+  late Future<QuestionDetail> futureQuestionDetail;
+  String providedText = '';
   var textSpeech = 'Click on Mic to Record';
   SpeechToText speechToText = SpeechToText();
   var isListening = false;
@@ -35,151 +34,174 @@ class _SpeakingTestState extends State<SpeakingTest> {
   void initState() {
     super.initState();
     checkMic();
+    futureQuestionDetail = fetchPracticeDetail(widget.questionGroupId);
+    futureQuestionDetail.then((questionDetail) {
+      setState(() {
+        providedText = questionDetail.sampleAnswer;
+      });
+    }).catchError((error) {
+      print('Failed to fetch practice detail: $error');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(100),
-        child: ClipPath(
-          clipper: CustomAppBar(),
-          child: AppBar(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Speaking Test',
-                  style: tFOnt(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            backgroundColor: const Color(0xFF0D0443),
-            leading: IconButton(
-              icon: const Icon(Icons.arrow_back_ios_new, color: Colors.white),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.only(left: 14, right: 14, top: 14),
-              height: 220,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1.0, color: Colors.black),
-                borderRadius: BorderRadius.circular(8),
-                color: Color(0xFFC0D6E8),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.4),
-                    spreadRadius: 1,
-                    blurRadius: 5,
-                    offset: Offset(0, 2),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(100),
+          child: ClipPath(
+            clipper: CustomAppBar(),
+            child: AppBar(
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Speaking Test',
+                    style: tFOnt(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
                   ),
                 ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  providedText,
-                  style: tFOnt(),
-                ),
+              backgroundColor: const Color(0xFF0D0443),
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios_new, color: Colors.white),
+                onPressed: () => Navigator.pop(context),
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 14, right: 14, top: 14),
-              height: 290,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1.0, color: Colors.black),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  textSpeech,
-                  style: tFOnt(),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 14, right: 14, top: 14),
-              height: 50,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1.0, color: Colors.black),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Accuracy: ${accuracy.toStringAsFixed(2)}%',
-                  style: tFOnt(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 40),
-            GestureDetector(
-              onTap: () async {
-                if (!isListening) {
-                  bool micAvailable = await speechToText.initialize();
-                  if (micAvailable) {
-                    setState(() {
-                      isListening = true;
-                    });
-
-                    speechToText.listen(
-                        listenFor: const Duration(seconds: 120),
-                        onResult: (result) {
-                          setState(() {
-                            textSpeech = result.recognizedWords;
-                            accuracy =
-                                calculateAccuracy(providedText, textSpeech);
-                            print("Provided Text: $providedText");
-                            print("Speech Text: $textSpeech");
-                            print("Akurasi: ${accuracy.toStringAsFixed(2)}%");
-                          });
-                        });
-                  }
-                } else {
-                  setState(() {
-                    isListening = false;
-                    speechToText.stop();
-                  });
-                }
-              },
-              child: CircleAvatar(
-                backgroundColor: mColor,
-                child: isListening
-                    ? const Icon(
-                        Icons.record_voice_over,
-                        color: Colors.white,
-                      )
-                    : const Icon(
-                        Icons.mic,
-                        color: Colors.white,
-                      ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text('Question Group ID: ${widget.questionGroupId}'),
-          ],
+          ),
         ),
-      ),
-    );
+        body: FutureBuilder<QuestionDetail>(
+            future: futureQuestionDetail,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Failed to load data'));
+              } else if (snapshot.hasData) {
+                return SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin:
+                            const EdgeInsets.only(left: 14, right: 14, top: 14),
+                        height: 220,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1.0, color: Colors.black),
+                          borderRadius: BorderRadius.circular(8),
+                          color: Color(0xFFC0D6E8),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.4),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            providedText,
+                            style: tFOnt(),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin:
+                            const EdgeInsets.only(left: 14, right: 14, top: 14),
+                        height: 290,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1.0, color: Colors.black),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            textSpeech,
+                            style: tFOnt(),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin:
+                            const EdgeInsets.only(left: 14, right: 14, top: 14),
+                        height: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1.0, color: Colors.black),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Accuracy: ${accuracy.toStringAsFixed(2)}%',
+                            style: tFOnt(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      GestureDetector(
+                        onTap: () async {
+                          if (!isListening) {
+                            bool micAvailable = await speechToText.initialize();
+                            if (micAvailable) {
+                              setState(() {
+                                isListening = true;
+                              });
+
+                              speechToText.listen(
+                                  listenFor: const Duration(seconds: 120),
+                                  onResult: (result) {
+                                    setState(() {
+                                      textSpeech = result.recognizedWords;
+                                      accuracy = calculateAccuracy(
+                                          providedText, textSpeech);
+                                      print("Provided Text: $providedText");
+                                      print("Speech Text: $textSpeech");
+                                      print(
+                                          "Akurasi: ${accuracy.toStringAsFixed(2)}%");
+                                    });
+                                  });
+                            }
+                          } else {
+                            setState(() {
+                              isListening = false;
+                              speechToText.stop();
+                            });
+                          }
+                        },
+                        child: CircleAvatar(
+                          backgroundColor: mColor,
+                          child: isListening
+                              ? const Icon(
+                                  Icons.record_voice_over,
+                                  color: Colors.white,
+                                )
+                              : const Icon(
+                                  Icons.mic,
+                                  color: Colors.white,
+                                ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text('Question Group ID: ${widget.questionGroupId}'),
+                    ],
+                  ),
+                );
+              } else {
+                return Center(child: Text('No data available'));
+              }
+            }));
   }
 
   double calculateAccuracy(String providedText, String textSpeech) {
