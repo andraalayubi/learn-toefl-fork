0aa66af1706ea510b95ca2f5488758f54d76ced4
fadhilahmargi <fadhilahmargi@gmail.com>
2024-06-02 00:50:22 +0700

    tampilan summary insyaAllah sudah aman

--- lib/pages/user/exercise/summary.dart
+++ lib/pages/user/exercise/summary.dart
@@ -11,7 +11,7 @@ class Summary extends StatefulWidget {
   final int incorrect;
   final QuestionDetail? detail; // tambahkan parameter ini
 
-  Summary({
+  const Summary({
     required this.score,
     required this.correct,
     required this.incorrect,
@@ -46,16 +46,16 @@ class _SummaryState extends State<Summary> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             'Summary',
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
             ),
           ),
-          backgroundColor: Color(0xFF0D0443),
+          backgroundColor: const Color(0xFF0D0443),
           leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
+            icon: const Icon(Icons.arrow_back_ios_new, color: Colors.white),
             onPressed: () => Navigator.pop(context),
           ),
         ),
@@ -68,7 +68,7 @@ class _SummaryState extends State<Summary> {
               ),
               Container(
                 height: MediaQuery.of(context).size.height / 1.7,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   image: DecorationImage(
                     image: AssetImage("assets/images/sparkle.png"),
                     fit: BoxFit.cover,
@@ -76,14 +76,14 @@ class _SummaryState extends State<Summary> {
                 ),
               ),
               Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: SingleChildScrollView(
                     child: Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          SizedBox(height: 50),
-                          Text(
+                          const SizedBox(height: 50),
+                          const Text(
                             'Congratulations!\nYou have completed all questions.',
                             textAlign: TextAlign.center,
                             style: TextStyle(
@@ -92,8 +92,8 @@ class _SummaryState extends State<Summary> {
                               color: Colors.white,
                             ),
                           ),
-                          SizedBox(height: 20),
-                          Text(
+                          const SizedBox(height: 20),
+                          const Text(
                             'Your score is:',
                             style: TextStyle(
                               fontSize: 20,
@@ -101,13 +101,13 @@ class _SummaryState extends State<Summary> {
                               color: Colors.white,
                             ),
                           ),
-                          SizedBox(height: 20),
+                          const SizedBox(height: 20),
                           Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Container(
-                                  padding: EdgeInsets.all(20),
+                                  padding: const EdgeInsets.all(20),
                                   decoration: BoxDecoration(
                                     color: Colors.white,
                                     borderRadius: BorderRadius.circular(16),
@@ -116,24 +116,24 @@ class _SummaryState extends State<Summary> {
                                         color: Colors.grey.withOpacity(0.5),
                                         spreadRadius: 5,
                                         blurRadius: 7,
-                                        offset: Offset(0, 3),
+                                        offset: const Offset(0, 3),
                                       ),
                                     ],
                                   ),
                                   child: Text(
                                     '${widget.score}',
-                                    style: TextStyle(
-                                      fontSize: 60,
+                                    style: const TextStyle(
+                                      fontSize: 73,
                                       fontWeight: FontWeight.bold,
                                       color: Colors.black,
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: 20),
+                                const SizedBox(width: 20),
                                 Column(
                                   children: [
                                     Container(
-                                      padding: EdgeInsets.all(16),
+                                      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
                                       decoration: BoxDecoration(
                                         color: Colors.green[100],
                                         borderRadius: BorderRadius.circular(16),
@@ -142,22 +142,22 @@ class _SummaryState extends State<Summary> {
                                             color: Colors.grey.withOpacity(0.5),
                                             spreadRadius: 5,
                                             blurRadius: 7,
-                                            offset: Offset(0, 3),
+                                            offset: const Offset(0, 3),
                                           ),
                                         ],
                                       ),
                                       child: Text(
                                         '${widget.correct} Correct',
                                         style: TextStyle(
-                                          fontSize: 16,
+                                          fontSize: 18,
                                           fontWeight: FontWeight.bold,
                                           color: Colors.green[700],
                                         ),
                                       ),
                                     ),
-                                    SizedBox(height: 10),
+                                    const SizedBox(height: 10),
                                     Container(
-                                      padding: EdgeInsets.all(16),
+                                      padding: const EdgeInsets.all(16),
                                       decoration: BoxDecoration(
                                         color: Colors.red[100],
                                         borderRadius: BorderRadius.circular(16),
@@ -166,20 +166,20 @@ class _SummaryState extends State<Summary> {
                                             color: Colors.grey.withOpacity(0.5),
                                             spreadRadius: 5,
                                             blurRadius: 7,
-                                            offset: Offset(0, 3),
+                                            offset: const Offset(0, 3),
                                           ),
                                         ],
                                       ),
                                       child: Text(
                                         '${widget.incorrect} Incorrect',
                                         style: TextStyle(
-                                          fontSize: 16,
+                                          fontSize: 18,
                                           fontWeight: FontWeight.bold,
                                           color: Colors.red[700],
                                         ),
                                       ),
                                     ),
-                                    SizedBox(height: 20),
+                                    const SizedBox(height: 20),
                                   ],
                                 ),
                               ]),
@@ -196,26 +196,36 @@ class _SummaryState extends State<Summary> {
                     // ),
                   ))
             ]),
+            const SizedBox(height: 20),
             ListView.builder(
               shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: widget.detail?.questions.length ?? 0,
               itemBuilder: (context, index) {
                 final question = widget.detail?.questions[index];
                 if (question != null) {
-                  return QuestionBox(
-                    questionNumber: index + 1,
-                    question: question.questionText,
-                    correctAnswer: question.correctAnswer,
-                    options: question.answerOptions,
-                    selectedOption: question.userAnswer,
-                    onSelect: (option) {
-                      // Tidak melakukan apa-apa karena ini adalah halaman ringkasan
-                    },
-                    isSummary: true,
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width,
+                      ),
+                      child: QuestionBox(
+                        questionNumber: index + 1,
+                        question: question.questionText,
+                        correctAnswer: question.correctAnswer,
+                        options: question.answerOptions,
+                        selectedOption: question.userAnswer,
+                        onSelect: (option) {
+                          // Tidak melakukan apa-apa karena ini adalah halaman ringkasan
+                        },
+                        isSummary: true,
+                      ),
+                    ),
                   );
                 } else {
-                  return SizedBox
+                  return const SizedBox
                       .shrink(); // Mengembalikan widget kosong jika question adalah null
                 }
               },
