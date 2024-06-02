876eb40a8567df69c570f9b7e40963dfb0e6d6d8
argyadwi <argyawoles@gmail.com>
2024-06-02 22:58:39 +0700

    implement fullscreen player

--- lib/pages/user/pages/video.dart
+++ lib/pages/user/pages/video.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:learn_toefl/models/video.dart';
 import 'package:learn_toefl/utilities.dart';
@@ -10,11 +11,12 @@ class VideoPage extends StatefulWidget {
   final int idCategory;
   final List<Video> video;
 
-  const VideoPage(
-      {super.key,
-      required this.id,
-      required this.video,
-      required this.idCategory});
+  const VideoPage({
+    Key? key,
+    required this.id,
+    required this.video,
+    required this.idCategory,
+  }) : super(key: key);
 
   @override
   State<VideoPage> createState() => _VideoPageState();
@@ -26,6 +28,7 @@ class _VideoPageState extends State<VideoPage> {
   late String title;
   late String urlImage;
   late List<Video> allVideos;
+  late YoutubePlayerController _controller;
 
   @override
   void initState() {
@@ -34,6 +37,13 @@ class _VideoPageState extends State<VideoPage> {
     allVideos = widget.video;
     id = widget.id;
     title = getTitle();
+    _controller = YoutubePlayerController(
+      initialVideoId: '',
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
   }
 
   String getTitle() {
@@ -52,230 +62,207 @@ class _VideoPageState extends State<VideoPage> {
 
   @override
   Widget build(BuildContext context) {
-    final otherVideos =
-        allVideos?.where((v) => v.id != id).take(3).toList() ?? [];
+    final otherVideos = allVideos.where((v) => v.id != id).take(3).toList();
 
     return FutureBuilder<Video>(
-        future: _videoData,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return Scaffold(
-              appBar: AppBar(
-                surfaceTintColor: Colors.transparent,
-                title: Column(
-                  children: [
-                    Text(
-                      title,
-                      style: tFOnt(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                      ),
+      future: _videoData,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              surfaceTintColor: Colors.transparent,
+              title: Column(
+                children: [
+                  Text(
+                    title,
+                    style: tFOnt(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      snapshot.data!.name,
-                      style: tFOnt(
-                        color: Colors.white,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-                centerTitle: true,
-                backgroundColor: const Color(0xFF0D0443),
-                leading: Padding(
-                  padding: const EdgeInsets.only(left: 22.0),
-                  child: IconButton(
-                    icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
                   ),
+                  Text(
+                    snapshot.data!.name,
+                    style: tFOnt(
+                      color: Colors.white,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+              centerTitle: true,
+              backgroundColor: const Color(0xFF0D0443),
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 22.0),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
               ),
-              body: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    ClipPath(
-                      // clipper: MyClipper(),
-                      child: Container(
-                        height: 310,
-                        width: double.infinity,
-                        decoration: const BoxDecoration(
-                          color: Color(0xFF0D0443),
+            ),
+            body: LayoutBuilder(
+              builder: (context, constraints) {
+                return YoutubePlayerBuilder(
+                  player: YoutubePlayer(
+                    aspectRatio: 16 / 9,
+                    controller: _controller,
+                    showVideoProgressIndicator: true,
+                    onReady: () {
+                      _controller.load(snapshot.data?.url ?? '');
+                      _controller.addListener(() {
+                        if (_controller.value.isFullScreen) {
+                          SystemChrome.setEnabledSystemUIMode(
+                            SystemUiMode.manual,
+                            overlays: [],
+                          );
+                        } else {
+                          SystemChrome.setEnabledSystemUIMode(
+                            SystemUiMode.manual,
+                            overlays: SystemUiOverlay.values,
+                          );
+                        }
+                      });
+                    },
+                  ),
+                  builder: (context, player) {
+                    return SingleChildScrollView(
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minHeight: constraints.maxHeight,
                         ),
-                        child: YoutubePlayerBuilder(
-                          player: YoutubePlayer(
-                            controller: YoutubePlayerController(
-                              initialVideoId: snapshot.data?.url ?? '',
-                              flags: const YoutubePlayerFlags(
-                                autoPlay: false,
-                                mute: false,
-                              ),
-                            ),
-                            showVideoProgressIndicator: true,
-                            aspectRatio:
-                                16 / 9, // Rasio aspek 16:9 (lebar/tinggi)
-                          ),
-                          builder: (context, player) {
-                            return Column(
-                              children: [
-                                // some widgets
-                                player,
-                                //some other widgets
-                              ],
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 100,
-                    ),
-                    Transform.translate(
-                      offset: const Offset(0, -80),
-                      child: Container(
-                        width: 360,
-                        padding: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF0D0443),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Row(
+                        child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Explore more ',
-                                    style: tFOnt(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  Text(
-                                    'lesson videos now!',
-                                    style: tFOnt(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ],
+                          children: <Widget>[
+                            Container(
+                              height: constraints.maxWidth * (9 / 16),
+                              width: double.infinity,
+                              decoration: const BoxDecoration(
+                                color: Color(0xFF0D0443),
                               ),
+                              child: player ?? SizedBox(), // Handle null player
                             ),
-                            const SizedBox(height: 40),
+                            SizedBox(height: 35),
                             Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Image.asset(
-                                urlImage,
-                                width: 80,
-                                height: 80,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Transform.translate(
-                      offset: const Offset(0, -65),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 26.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              'More Videos',
-                              style: tFOnt(
-                                  fontSize: 14, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Transform.translate(
-                      offset: const Offset(0, -55),
-                      child: Column(
-                        children: [
-                          ...otherVideos.map(
-                            (v) => GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => VideoPage(
-                                            id: v.id,
-                                            video: allVideos,
-                                            idCategory: widget.idCategory)));
-                              },
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 18.0),
                               child: Container(
-                                height: 60,
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 26, vertical: 10),
+                                width: double.infinity,
+                                padding: const EdgeInsets.all(20),
                                 decoration: BoxDecoration(
+                                  color: const Color(0xFF0D0443),
                                   borderRadius: BorderRadius.circular(12),
-                                  color:
-                                      Theme.of(context).scaffoldBackgroundColor,
-                                  border: Border.all(
-                                      width: 1.0, color: Colors.black),
                                 ),
-                                child: Row(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    const Padding(
-                                      padding: EdgeInsets.only(left: 8.0),
-                                      child: Icon(Icons.play_circle_outline),
-                                    ),
-                                    Expanded(
-                                      child: Padding(
-                                        padding:
-                                            const EdgeInsets.only(left: 12.0),
-                                        child: Text(
-                                          v.name,
-                                          style: tFOnt(
-                                              fontWeight: FontWeight.bold),
-                                          maxLines: 1,
-                                        ),
+                                    Text(
+                                      'Explore more $title videos now!',
+                                      style: tFOnt(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
                                       ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Image.asset(
+                                      urlImage,
+                                      width: 80,
+                                      height: 80,
+                                      fit: BoxFit.cover,
                                     ),
                                   ],
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 20),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Text(
+                                'More Videos',
+                                style: tFOnt(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Column(
+                              children: [
+                                ...otherVideos.map(
+                                  (v) => GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => VideoPage(
+                                            id: v.id,
+                                            video: allVideos,
+                                            idCategory: widget.idCategory,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    child: Container(
+                                      height: 60,
+                                      margin: const EdgeInsets.symmetric(
+                                        horizontal: 20,
+                                        vertical: 10,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: Theme.of(context)
+                                            .scaffoldBackgroundColor,
+                                        border: Border.all(
+                                          width: 1.0,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 8.0),
+                                            child:
+                                                Icon(Icons.play_circle_outline),
+                                          ),
+                                          Expanded(
+                                            child: Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 12.0),
+                                              child: Text(
+                                                v.name,
+                                                style: tFOnt(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                                maxLines: 1,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-            );
-          }
-        });
-  }
-}
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 180);
-    path.quadraticBezierTo(
-        size.width / 2, size.height - 100, size.width, size.height - 180);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
+                    );
+                  },
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
   }
 }
